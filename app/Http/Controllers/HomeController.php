a3393cddbbad407e4b48c6b91606f241d61d3e23
Ar Roso <Ar Roso>
2017-03-08 21:34:21 +0800

    [access-20] added change password ui and route

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,4 +25,14 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+    /**
+     * Show the change password form.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function changePassword()
+    {
+        return view('auth/passwords/change');
+    }
 }
