0fcbce7262234408a22f2be7a2e763f1212d4bb3
Lito Cabornay <litocabornay91@gmail.com>
2017-03-12 18:16:17 +0800

    [access-15] Removed commented

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,22 +3,9 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <!-- <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
-                </div>
-            </div>
-        </div> -->
-
-
         <div class="col-lg-12">
             <h1 class="page-header">Sample Dashboard Page</h1>
         </div>
-        <!-- /.col-lg-12 -->
-
     </div>
 
     <div class="row">
