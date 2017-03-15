3ab83b51da98c4e30c8967b204fc43f85595d72c
Clint James <cjestal@gmail.com>
2017-03-15 08:46:15 -0700

    [access-61] modified dashboard to display sidebar, company details, and user details

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,103 +1,52 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-lg-12">
-            <h1 class="page-header">Sample Dashboard Page</h1>
-        </div>
-    </div>
+    <div class="container">
+        <div class="row">
+            @include('admin.sidebar')
 
-    <div class="row">
-
-        <div class="col-sm-12">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h3 class="panel-title">Announcements </h3>
+            <div class="col-md-9">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">Announcements</div>
+                    <div class="panel-body">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </div>
                 </div>
-                <div class="panel-body">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+                <div class="col-md-6 nopadding">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Company Details</div>
+                        <div class="panel-body">Announcements go here!</div>
+                    </div>
+                </div>
+
+                <div class="col-md-6 nopadding">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">User Details</div>
+                        <div class="panel-body">
+                            <div class="table-responsive">
+                                <table class="table table-borderless">
+                                    <thead>
+                                        <tr><th>First Name</th><td> {{ $userInfo->firstname }} </td></tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr><th>Last Name</th><td> {{ $userInfo->lastname }} </td></tr>
+                                        <tr><th>Middle Name</th><td> {{ $userInfo->middlename }} </td></tr>
+                                        <tr><th>Email</th><td> {{ $userInfo->email }} </td></tr>
+                                        <tr><th>Telephone No</th><td> {{ $userInfo->tel_num }} </td></tr>
+                                        <tr><th>Mobile No</th><td> {{ $userInfo->mobile_num }} </td></tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-
-    <div class="row">
-        <div class="col-sm-6">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h3 class="panel-title">Users</h3>
-                </div>
-                <div class="panel-body">
-                    <table class="table ">
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Email</th>
-                                <th>Address</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>John</td>
-                                <td>john@gmail.com</td>
-                                <td>London, UK</td>
-                            </tr>
-                            <tr>
-                                <td>Andy</td>
-                                <td>andygmail.com</td>
-                                <td>Merseyside, UK</td>
-                            </tr>
-                            <tr>
-                                <td>Frank</td>
-                                <td>frank@gmail.com</td>
-                                <td>Southampton, UK</td>
-                            </tr>
-                        </tbody>
-                    </table>            
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-6">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h3 class="panel-title">Supplier Data </h3>
-                </div>
-                <div class="panel-body">
-                    <table class="table table-bordered">
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Email</th>
-                                <th>Address</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>John</td>
-                                <td>john@gmail.com</td>
-                                <td>London, UK</td>
-                            </tr>
-                            <tr>
-                                <td>Andy</td>
-                                <td>andygmail.com</td>
-                                <td>Merseyside, UK</td>
-                            </tr>
-                            <tr>
-                                <td>Frank</td>
-                                <td>frank@gmail.com</td>
-                                <td>Southampton, UK</td>
-                            </tr>
-                        </tbody>
-                    </table>            
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
